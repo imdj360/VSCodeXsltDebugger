2e84c98533df6f93990e8f1096243d5f5aa96b28
Daniel Jonathan <imdj360@users.noreply.github.com>
2026-03-26 17:55:31 +0100

    fix: e2e test asserts on output file instead of DAP custom event

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,4 +1,5 @@
 import * as assert from 'assert';
+import * as fs from 'fs';
 import * as vscode from 'vscode';
 import * as path from 'path';
 
@@ -7,48 +8,45 @@ suite('XSLT Debugger End-to-End', function () {
 
   test('should launch and run XSLT debug session with inline C#', async () => {
     const workspaceFolder = vscode.workspace.workspaceFolders?.[0].uri.fsPath || '';
+    const stylesheet = path.join(workspaceFolder, 'TestData', 'Integration', 'xslt', 'compiled', 'sample-inline-cs.xslt');
+    const outFile = path.join(workspaceFolder, 'TestData', 'Integration', 'xslt', 'compiled', 'out', 'sample-inline-cs.out.xml');
+
+    // Remove stale output file before run
+    if (fs.existsSync(outFile)) { fs.unlinkSync(outFile); }
+
     const debugConfig: vscode.DebugConfiguration = {
       type: 'xslt',
       request: 'launch',
       name: 'Debug XSLT (.NET Hybrid)',
-      stylesheet: path.join(workspaceFolder, 'TestData', 'Integration', 'xslt', 'compiled', 'sample-inline-cs.xslt'),
+      stylesheet,
       xml: path.join(workspaceFolder, 'TestData', 'Integration', 'xml', 'sample.xml'),
       engine: 'compiled'
     };
 
     let sessionStarted = false;
     let sessionTerminated = false;
-    let outputReceived = '';
 
     const startListener = vscode.debug.onDidStartDebugSession(session => {
-      if (session.name === debugConfig.name) {
-        sessionStarted = true;
-      }
+      if (session.name === debugConfig.name) { sessionStarted = true; }
     });
     const termListener = vscode.debug.onDidTerminateDebugSession(session => {
-      if (session.name === debugConfig.name) {
-        sessionTerminated = true;
-      }
-    });
-    const outputListener = vscode.debug.onDidReceiveDebugSessionCustomEvent(e => {
-      if (e.session.name === debugConfig.name && e.event === 'output') {
-        outputReceived += e.body.output;
-      }
+      if (session.name === debugConfig.name) { sessionTerminated = true; }
     });
 
     await vscode.debug.startDebugging(vscode.workspace.workspaceFolders?.[0], debugConfig);
 
-    // Wait for session to start and terminate
+    // Wait for session to terminate
     for (let i = 0; i < 60 && !sessionTerminated; i++) {
       await new Promise(res => setTimeout(res, 1000));
     }
 
     startListener.dispose();
     termListener.dispose();
-    outputListener.dispose();
 
     assert.ok(sessionStarted, 'Debug session should start');
     assert.ok(sessionTerminated, 'Debug session should terminate');
-    assert.match(outputReceived, /Hello, World!/);
+    assert.ok(fs.existsSync(outFile), `Output file should be written at ${outFile}`);
+    const output = fs.readFileSync(outFile, 'utf8');
+    assert.match(output, /Hello, World!/, 'Transform output should contain Hello, World!');
   });
 });
